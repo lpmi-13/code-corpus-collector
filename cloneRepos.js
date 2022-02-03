607a1779ac3d11325f55bad065510d44d93c8264
Adam Leskis <leskis@gmail.com>
2022-02-03 11:38:51 +0000

    use shallow clone

--- cloneRepos.js
+++ cloneRepos.js
@@ -1,15 +1,20 @@
-const lineReader = require('line-reader');
-const fs = require('fs');
-const clone = require('git-clone');
+import lineReader from 'line-reader';
+import fs from 'fs';
+import clone from 'git-clone';
 
 function processAllRepos(repos) {
   repos.forEach(cloneLocally);
 }
 
-const cloneLocally = gitRepo => {
+const cloneLocally = async gitRepo => {
+
+  const options = {
+    shallow: true
+  };
+
   const repoName = gitRepo.split('/').slice(-2).join('/');
   // TODO: check here to see if the file path already exists and skip if so
-  clone(gitRepo, `javascript/${repoName}`, () => console.log(`cloned ${repoName}`));
+  await clone(gitRepo, `javascript/${repoName}`, options, () => console.log(`cloned ${repoName}`));
 }
 
 const repos = fs.readFileSync('javascript-results.txt')
