804201e95720b3fdfd51f6d4fecbc0b689757d3a
Adam Leskis <leskis@gmail.com>
2022-06-10 00:18:01 +0100

    with some working hacks

--- javascript/extractFunctions.js
+++ javascript/extractFunctions.js
@@ -3,14 +3,9 @@ import { join } from 'path'
 import * as parser from "@babel/parser";
 import _traverse from "@babel/traverse";
 const traverse = _traverse.default;
-import { program } from 'commander';
 import { MongoClient } from 'mongodb';
 
-program
-  .option('-v, --verbose', 'verbose log level', false)
-
-program.parse();
-const options = program.opts();
+const args = process.argv.slice(2);
 
 const DATABASE_NAME = 'javascript';
 const COLLECTION_NAME = 'functions';
@@ -19,65 +14,6 @@ const mongoUri = `mongodb://localhost:27017/${DATABASE_NAME}`;
 const mongoClient = new MongoClient(mongoUri);
 await mongoClient.connect();
 
-const collection = mongoClient.db().collection(`${COLLECTION_NAME}`);
-
-console.log('starting to process files...')
-
-for await (const file of allFiles('repositories')) {
-  if (file.endsWith('.js')) {
-    const buffer = await readFile(file)
-    const code = buffer.toString()
-
-    for (const fn of extractFunctions(code)) {
-
-      const originalRepoName = file
-                                 .split('/')
-                                 .slice(1,3)
-                                 .join('/')
-      const filePath = file.split('/')
-
-      const functionLines = fn.split('\n')
-
-      const fullRepoUrl = `https://github.com/${originalRepoName}`
-
-      const codeObject = {
-        "type": "functions",
-        "project_source": fullRepoUrl,
-        // no straightforward way to get this yet, but we would need
-        // it for a direct link to the line in github
-        "direct_link_to_file_line": "",
-        "contents": {
-          "total_lines": functionLines.length,
-          "lines": functionLines.map((fn, i) => {
-             return {"line_number": i+1, "line_content": fn}
-          }),
-        },
-      };
-
-      if (options.verbose) {
-        console.log(`inserting ${fullRepoUrl}...`)
-      }
-      collection.insertOne(codeObject);
-    }
-  }
-}
-
-/** recursively get all files */
-async function* allFiles(directory) {
-  for await (const file of await opendir(directory)) {
-    const path = join(directory, file.name)
-
-    if (file.isFile()) {
-      yield path
-    }
-
-    if (file.isDirectory()) {
-      for await (const entry of allFiles(path)) {
-        yield entry;
-      }
-    }
-  }
-}
 
 /** Find function definitions from a js file string */
 function extractFunctions(code) {
@@ -99,3 +35,68 @@ function extractFunctions(code) {
   }
 }
 
+/** recursively get all files */
+async function* allFiles(directory) {
+  for await (const file of await opendir(directory)) {
+    const path = join(directory, file.name)
+
+    if (file.isFile()) {
+      yield path
+    }
+
+    if (file.isDirectory()) {
+      for await (const entry of allFiles(path)) {
+        yield entry;
+      }
+    }
+  }
+}
+
+async function grabFunctions(repoName) {
+  console.log(`starting to process files for ${repoName}...`)
+
+  const collection = mongoClient.db().collection(`${COLLECTION_NAME}`);
+
+  for await (const file of allFiles(repoName)) {
+    if (file.endsWith('.js')) {
+      const buffer = await readFile(file)
+      const code = buffer.toString()
+
+      for (const fn of extractFunctions(code)) {
+
+        const originalRepoName = file
+                                   .split('/')
+                                   .slice(1,3)
+                                   .join('/')
+        const filePath = file.split('/')
+
+        const functionLines = fn.split('\n')
+
+        const fullRepoUrl = `https://github.com/${originalRepoName}`
+
+        const codeObject = {
+          "type": "functions",
+          "project_source": fullRepoUrl,
+          // no straightforward way to get this yet, but we would need
+          // it for a direct link to the line in github
+          "direct_link_to_file_line": "",
+          "contents": {
+            "total_lines": functionLines.length,
+            "lines": functionLines.map((fn, i) => {
+               return {"line_number": i+1, "line_content": fn}
+            }),
+          },
+        };
+
+        console.log(`inserting ${fullRepoUrl}...`)
+
+        collection.insertOne(codeObject);
+      }
+    }
+  }
+  // the mongo client wasn't playing nice with closing connections, so welcome to
+  // the world of hacks!
+  process.exit(0)
+}
+
+await grabFunctions(args[0])
--- typescript/extractFunctions.js
+++ typescript/extractFunctions.js
@@ -5,6 +5,8 @@ import _traverse from "@babel/traverse";
 const traverse = _traverse.default;
 import { MongoClient } from 'mongodb';
 
+const args = process.argv.slice(2);
+
 const DATABASE_NAME = 'typescript';
 const COLLECTION_NAME = 'functions';
 
@@ -12,42 +14,24 @@ const mongoUri = `mongodb://localhost:27017/${DATABASE_NAME}`;
 const mongoClient = new MongoClient(mongoUri);
 await mongoClient.connect();
 
-const collection = mongoClient.db().collection(`${COLLECTION_NAME}`);
 
-for await (const file of allFiles('repositories')) {
-  if (file.endsWith('.ts')) {
-    const buffer = await readFile(file)
-    const code = buffer.toString()
+/** Find function definitions from a js file string */
+function extractFunctions(code) {
+  try {
+    const fns = []
+    const ast = parser.parse(code, { sourceType: "module" })
 
-    for (const fn of extractFunctions(code)) {
+    traverse(ast, {
+      enter({ node: { type, start, end } }) {
+        if (type === 'FunctionDeclaration') {
+          fns.push(code.slice(start, end))
+        }
+      }
+    })
 
-      const originalRepoName = file
-                                 .split('/')
-                                 .slice(1,3)
-                                 .join('/')
-      const filePath = file.split('/')
-
-      const functionLines = fn.split('\n')
-
-      const fullRepoUrl = `https://github.com/${originalRepoName}`
-
-      const codeObject = {
-        "type": "functions",
-        "project_source": fullRepoUrl,
-        // no straightforward way to get this yet, but we would need
-        // it for a direct link to the line in github
-        "direct_link_to_file_line": "",
-        "contents": {
-          "total_lines": functionLines.length,
-          "lines": functionLines.map((fn, i) => {
-             return {"line_number": i+1, "line_content": fn}
-          }),
-        },
-      };
-
-      console.log(`inserting ${fullRepoUrl}...`)
-      collection.insertOne(codeObject);
-    }
+    return fns;
+  } catch {
+    return []
   }
 }
 
@@ -68,30 +52,51 @@ async function* allFiles(directory) {
   }
 }
 
-/** Find function definitions from a js file string */
-function extractFunctions(code) {
-  try {
-    const fns = []
-    const ast = parser.parse(code, {
-      sourceType: "module",
-      plugins: [
-        "typescript",
-      ]
-    });
+async function grabFunctions(repoName) {
+  console.log(`starting to process files for ${repoName}...`)
 
-    traverse(ast, {
-      enter({ node: { type, start, end } }) {
-        if (type === 'FunctionDeclaration') {
-          fns.push(code.slice(start, end))
-        }
+  const collection = mongoClient.db().collection(`${COLLECTION_NAME}`);
+
+  for await (const file of allFiles(repoName)) {
+    if (file.endsWith('.js')) {
+      const buffer = await readFile(file)
+      const code = buffer.toString()
+
+      for (const fn of extractFunctions(code)) {
+
+        const originalRepoName = file
+                                   .split('/')
+                                   .slice(1,3)
+                                   .join('/')
+        const filePath = file.split('/')
+
+        const functionLines = fn.split('\n')
+
+        const fullRepoUrl = `https://github.com/${originalRepoName}`
+
+        const codeObject = {
+          "type": "functions",
+          "project_source": fullRepoUrl,
+          // no straightforward way to get this yet, but we would need
+          // it for a direct link to the line in github
+          "direct_link_to_file_line": "",
+          "contents": {
+            "total_lines": functionLines.length,
+            "lines": functionLines.map((fn, i) => {
+               return {"line_number": i+1, "line_content": fn}
+            }),
+          },
+        };
+
+        console.log(`inserting ${fullRepoUrl}...`)
+
+        collection.insertOne(codeObject);
       }
-    })
-
-    return fns;
-  } catch {
-    return []
+    }
   }
+  // the mongo client wasn't playing nice with closing connections, so welcome to
+  // the world of hacks!
+  process.exit(0)
 }
 
-console.log('closing mongo connection...');
-mongoClient.close();
+await grabFunctions(args[0])
